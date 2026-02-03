b3513f9504a749be5d47b5bed98c1457516373bd
Milan Straka <milan@strakovi.com>
2026-02-03 10:27:52 +0100

    Include the training data preprocessing via SingleParent udapi block.

--- data/get.sh
+++ data/get.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if ! command -v udapy >/dev/null 2>&1; then
+  echo "The 'udapy' command was not found. You can install it for example by creating" >&2
+  echo "a Python virtual environment, activating it, and running 'pip install udapi'." >&2
+  exit 1
+fi
+
 (
 mkdir original_data
 cd original_data
@@ -14,7 +20,8 @@ unzip unc-gold-minidev.zip
 )
 
 for tb in ca_ancora cs_pcedt cs_pdt cs_pdtsc cu_proiel es_ancora grc_proiel hu_korkor hu_szegedkoref pl_pcc tr_itcc; do
-  mv original_data/$tb-*.conllu .
+  udapy -s corefud.SingleParent <original_data/$tb-corefud-train.conllu > $tb-corefud-train.conllu
+  mv original_data/$tb-corefud-minidev.conllu .
 done
 
 rm -rf original_data
